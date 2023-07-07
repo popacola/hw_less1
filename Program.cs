c52a2afbf23e1e839bebf268327f43a1983517e8
Ilya Belikh <main.225@yandex.ru>
2023-07-07 17:39:05 +0400

    домашка

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿/* Задача 2
+int first_number = Convert.ToInt32(Console.ReadLine());
+int second_number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Math.Max(first_number, second_number));
+*/
+
+/* Задача 4
+int first_number = Convert.ToInt32(Console.ReadLine());
+int second_number = Convert.ToInt32(Console.ReadLine());
+int third_number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Math.Max(first_number, Math.Max(second_number,third_number)));
+*/
+
+/* Задача 6
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0) 
+{
+    Console.WriteLine("even");
+}
+else
+{
+    Console.WriteLine("odd");
+}
+*/
+
+/* Задача 8
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; i <= N; i++)
+{
+    if (i % 2 == 0) 
+    {
+        Console.WriteLine(i);
+    }
+}
+*/
